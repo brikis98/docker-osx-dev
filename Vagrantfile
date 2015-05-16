448c14cfdfe90e921d3c48edf1230b42727af12d
Yevgeniy Brikman <brikis98@gmail.com>
2015-05-16 16:23:43 -0700

    Initial version of setup.sh and Vagrantfile

---
+++ Vagrantfile
@@ -0,0 +1,97 @@
+DOCKER_COMPOSE_FILE = "docker-compose.yml"
+DOCKER_COMPOSE_VOLUMES_KEY = "volumes"
+DOCKER_COMPOSE_VOLUMES_SEPARATOR = ":"
+
+VAGRANTFILE_API_VERSION = "2"
+VAGRANT_ROOT = File.dirname(__FILE__)
+VAGRANT_FOLDER_NAME = File.basename(VAGRANT_ROOT)
+DEFAULT_FOLDERS_TO_SYNC = {:src => VAGRANT_ROOT, :dest => VAGRANT_ROOT}  
+
+# Set default provider
+ENV['VAGRANT_DEFAULT_PROVIDER'] = 'virtualbox'
+
+# Parse .gitignore style files and return the entries within
+def parse_ignore_file(file)
+  if File.exist?(file)
+    IO.read(file)
+      .split("\n")
+      .reject{|line| line.empty? || line.start_with?("#")}
+  else
+    []
+  end
+end
+
+# Determine the folders to sync to the VM based on the volumes specified in the
+# docker-compose file. If this project isn't using docker-compose, sync the
+# current directory.
+def folders_to_sync
+  docker_compose_file = File.join(VAGRANT_ROOT, DOCKER_COMPOSE_FILE)
+  if File.exist?(docker_compose_file)
+    require 'yaml'
+    docker_compose_config = YAML::load_file(docker_compose_file)
+    volumes = docker_compose_config.values.map { |container| container.fetch(DOCKER_COMPOSE_VOLUMES_KEY, []) }.flatten
+    local_volumes = volumes.select { |volume| volume.include?(DOCKER_COMPOSE_VOLUMES_SEPARATOR) }
+    local_volumes.map do |volume| 
+      src, _ = volume.split(DOCKER_COMPOSE_VOLUMES_SEPARATOR)
+      absolute_src = File.expand_path(src)
+      # Note: destination needs to be same as source. This way, /foo on the host
+      # machine (OS X) ends up at /foo on the VM. This is because the volumes
+      # entries in docker-compose.yml and the volumes specified in the -v flag
+      # to docker are looking for folders on the VM.
+      {:src => absolute_src, :dest => absolute_src}
+    end
+  else
+    DEFAULT_FOLDERS_TO_SYNC
+  end
+end
+
+Vagrant.require_version ">= 1.6.3"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.define "boot2docker"
+
+  config.vm.box = "blinkreaction/boot2docker"
+  config.vm.box_version = "1.6.0"
+  config.vm.box_check_update = false
+
+  # When syncing, exclude any files in .gitignore or .dockerignore
+  excludes = (parse_ignore_file(".gitignore") + parse_ignore_file(".dockerignore")).uniq
+
+  # Sync folders using rsync
+  folders_to_sync.each do |folder|
+    config.vm.synced_folder folder[:src], folder[:dest],
+      type: "rsync",
+      rsync__exclude: excludes,
+      rsync__args: ["--verbose", "--archive", "--delete", "-z", "--chmod=ugo=rwX"]
+  end
+
+  config.ssh.insert_key = false
+
+  config.vm.provider "virtualbox" do |v|
+    v.gui = false
+    v.name = VAGRANT_FOLDER_NAME + "_boot2docker"
+    v.cpus = 1
+    v.memory = 2048
+    # Necessary to ensure "sending build to context" runs quickly
+    v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+    v.customize ["modifyvm", :id, "--natdnsproxy1", "on"]    
+    v.customize ["modifyvm", :id, "--nictype1", "virtio"]
+  end
+
+  # Allow Mac OS X docker client to connect to Docker without TLS auth.
+  # https://github.com/deis/deis/issues/2230#issuecomment-72701992
+  config.vm.provision :shell do |s|
+    s.inline = <<-EOT
+      echo 'DOCKER_TLS=no' >> /var/lib/boot2docker/profile
+      /etc/init.d/docker restart
+    EOT
+  end  
+
+  # Adjust datetime after suspend and resume
+  config.vm.provision :shell do |s|
+    s.inline = <<-EOT
+      sudo /usr/local/bin/ntpclient -s -h pool.ntp.org
+      date
+    EOT
+  end  
+end
---
+++ setup.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+
+# set -e
+
+# Environment variable file constants
+readonly BASH_PROFILE="$HOME/.bash_profile"
+readonly BASH_RC="$HOME/.bashrc"
+readonly ZSH_RC="$HOME/.zshrc"
+
+# Docker environment variable constants
+readonly DOCKER_HOST_EXPORT="export DOCKER_HOST"
+readonly DOCKER_HOST="tcp://localhost:2375"
+
+# Url constants
+readonly HOSTS_FILE="/etc/hosts"
+readonly VAGRANT_HOST="192.168.10.10"
+readonly DOCKER_HOST_NAME="dockerhost"
+
+# Console colors
+readonly COLOR_INFO='\033[0;32m[INFO]'
+readonly COLOR_WARN='\033[1;33m[WARN]'
+readonly COLOR_ERROR='\033[0;31m[ERROR]'
+readonly COLOR_END='\033[0m'
+
+# Script constants
+readonly BIN_DIR="/usr/local/bin"
+readonly DOCKER_OSX_DEV_SCRIPT_NAME="docker-osx-dev"
+readonly DOCKER_OSX_DEV_URL="https://raw.githubusercontent.com/brikis98/docker-osx-dev/master/$DOCKER_OSX_DEV_SCRIPT_NAME"
+
+function log_info {
+  log "$1" $COLOR_INFO
+}
+
+function log_warn {
+  log "$1" $COLOR_WARN
+}
+
+function log_error {
+  log "$1" $COLOR_ERROR
+}
+
+function log {
+  local readonly message=$1
+  local readonly color=$2 || $COLOR_INFO
+  echo -e "${color} ${message}${COLOR_END}"
+}
+
+function brew_install {
+  local readonly package_name="$1"
+  local readonly readable_name="$2"
+  local readonly command_name="$3"
+  local readonly use_cask="$4"
+
+  if brew list $package_name > /dev/null 2>&1 ; then
+    log_warn "$readable_name is already installed by HomeBrew, skipping"
+  elif command -v $command_name > /dev/null 2>&1 ; then
+    log_warn "Found command $command_name, assuming $readable_name is already installed and skipping"
+  else
+    log_info "Installing $readable_name"
+    if [[ $use_cask ]]; then
+      brew cask install $package_name
+    else
+      brew install $package_name
+    fi
+  fi  
+}
+
+function vagrant_plugin_install {
+  local readonly plugin_name=$1
+  local readonly readable_name=$2
+
+  if vagrant plugin list | grep -q $plugin_name ; then
+    log_warn "$readable_name already installed, skipping"
+  else
+    log_info "Installing $readable_name"
+    vagrant plugin install $plugin_name
+  fi
+}
+
+function get_env_file {
+  if [[ -f "$BASH_RC" ]]; then 
+    echo "$BASH_RC"
+  elif [[ -f "$ZSH_RC" ]]; then
+    echo "$ZSH_RC"
+  else
+    echo "$BASH_PROFILE"
+  fi
+}
+
+function check_prerequisites {
+  command -v brew > /dev/null 2>&1 || { log_error "I require HomeBrew, but it's not installed. Aborting."; exit 1; }
+}
+
+function install_dependencies {
+  log_info "Installing dependencies"
+
+  log_info "Updating HomeBrew"
+  brew update
+
+  brew_install "caskroom/cask/brew-cask" "Cask" "" false
+  brew_install "virtualbox" "VirtualBox" "vboxmanage" true
+  brew_install "vagrant" "Vagrant" "vagrant" true
+  brew_install "docker" "Docker" "docker" false
+  brew_install "docker-compose" "Docker Compose" "docker-compose" false
+}
+
+function install_vagrant_plugins {
+  log_info "Installing Vagrant plugins"
+
+  vagrant_plugin_install vagrant-gatling-rsync "Vagrant Gatling Rsync"
+}
+
+function add_environment_variables {
+  log_info "Adding environment variables"
+
+  local readonly env_file=$(get_env_file)
+  local readonly docker_host_export="$DOCKER_HOST_EXPORT=$DOCKER_HOST"
+
+  if grep -q "$DOCKER_HOST_EXPORT" "$env_file" ; then
+    log_warn "$env_file already contains \"$DOCKER_HOST_EXPORT\", will not overwrite with $docker_host_export"
+  else
+    log_info "Adding $docker_host_export to $env_file"
+    echo "$docker_host_export" >> "$env_file"
+  fi  
+}
+
+function add_docker_host {
+  local readonly host_entry="$VAGRANT_HOST $DOCKER_HOST_NAME"
+  if grep -q "$DOCKER_HOST_NAME" "$HOSTS_FILE" ; then
+    log_warn "$HOSTS_FILE already contains $DOCKER_HOST_NAME, will not add entry \"$host_entry\""
+  else
+    log_info "Adding $host_entry to $HOSTS_FILE, will need sudo password!"
+    sudo echo '$host_entry' >> $HOSTS_FILE
+  fi
+}
+
+function install_local_scripts {
+  local readonly script_path="$BIN_DIR/$DOCKER_OSX_DEV_SCRIPT_NAME"
+  if [[ -f "$script_path" ]]; then
+    log_warn "$DOCKER_OSX_DEV_SCRIPT_NAME already exists, will not overwrite"
+  else
+    log_info "Adding $script_path"
+    curl -L "$DOCKER_OSX_DEV_URL" > "$script_path"
+    chmod +x "$script_path"
+  fi
+}
+
+check_prerequisites
+install_dependencies
+install_vagrant_plugins
+add_environment_variables
+#add_docker_host
+install_local_scripts
