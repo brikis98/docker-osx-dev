72a4132a003cec4cb72c50d38d33aad7b78a0f36
Yevgeniy Brikman <brikis98@gmail.com>
2015-05-19 12:25:41 -0700

    Add some logging to test script

--- test.sh
+++ test.sh
@@ -4,9 +4,34 @@
 
 set -e
 
+# Test file constants
 readonly TEST_FOLDER="test-project"
 readonly TEST_FILE="test-file"
 readonly TEST_FILE_CONTENTS="test file contents"
+
+# Console colors
+readonly COLOR_INFO='\033[0;3m[TEST_INFO]'
+readonly COLOR_WARN='\033[1;33m[TEST_WARN]'
+readonly COLOR_ERROR='\033[0;31m[TEST_ERROR]'
+readonly COLOR_END='\033[0m'
+
+function log_info {
+  log "$1" $COLOR_INFO
+}
+
+function log_warn {
+  log "$1" $COLOR_WARN
+}
+
+function log_error {
+  log "$1" $COLOR_ERROR
+}
+
+function log {
+  local readonly message=$1
+  local readonly color=$2 || $COLOR_INFO
+  echo -e "${color} ${message}${COLOR_END}"
+}
 
 function assert_equals {
   local readonly left=$1
@@ -19,33 +44,40 @@ function assert_equals {
 }
 
 function test_setup {
+  log_info "Testing setup.sh script"
   # We're just looking for the script to run without errors
   ./setup.sh  
 }
 
 function create_test_project {
+  log_info "Creating test project in $TEST_FOLDER"
   mkdir "$TEST_FOLDER"
   cd "$TEST_FOLDER"
   echo "$TEST_FILE_CONTENTS" > "$TEST_FILE"
 }
 
 function test_docker_osx_dev_start {
+  log_info "Running docker-osx-dev init and start. PWD = $(pwd)"
+  ls
   # We're just looking for the scripts to run without errors  
   docker-osx-dev init
   docker-osx-dev start
 }
 
 function test_docker_run {
+  log_info "Testing docker run with Alpine Linux image"
   local readonly out=$(docker run --rm gliderlabs/alpine:3.1 uname)
   assert_equals "$out" "Linux"  
 }
 
 function test_docker_mount {
+  log_info "Testing mounting a folder with Alpine Linux image"
   local readonly out=$(docker run --rm -v $(pwd):/src gliderlabs/alpine:3.1 cd /src && cat foo)
   assert_equals "$out" "$TEST_FILE_CONTENTS"
 }
 
 function test_docker_osx_dev_stop {
+  log_info "Testing docker-osx-dev stop"
   # We're just looking for the scripts to run without errors  
   docker-osx-dev stop
 }
